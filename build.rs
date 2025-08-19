2d4ece32ce678c19bca8b2cb3e1499518b78c1d1
crisbez <cris@digicom.bg>
2025-08-19 16:03:40 +0300

    fix(ci): vendored protoc for tonic-build

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Използвай вграден protoc, за да не зависи от системен пакет
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()?;
+    std::env::set_var("PROTOC", protoc_path);
+
     tonic_build::configure()
         .build_server(true)
         .compile(&["proto/atlas4d.proto"], &["proto"])?;
